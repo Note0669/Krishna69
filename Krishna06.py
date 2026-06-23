3e6c1b4ca11bb48d8b2234160b9598d6739fc911
Note0669 <164133@wsk.ac.th>
2026-06-23 03:04:43 +0000

    program02.py

---
+++ Krishna06.py
@@ -0,0 +1,5 @@
+width = float(input("enter width: ")) #กรอกค่าความกว้างของข้อมูล
+length = float(input("enter length: ")) #กรอกค่าความยาวของข้อมูล
+area = width * length #แปลงผลของพื้นที่สี่เหลี่ยมมุมฉาก
+print(f"Rectangle area is: {area:.2f}") #เปลี่ยนค่าทศนิยมให้เหลือ2ตำแหน่ง
+print("author by Krishna")
